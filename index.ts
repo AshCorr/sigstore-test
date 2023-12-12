f6cbd04efeb1874ef9c09655c33d24cf3048b9d3
Ashleigh Carr <ashcorr20@gmail.com>
2023-12-12 15:58:29 +0000

    Use client instead of low level packages

--- index.ts
+++ index.ts
@@ -1,41 +1,17 @@
-import { bundleToJSON } from "@sigstore/bundle";
-import {
-  CIContextProvider,
-  DSSEBundleBuilder,
-  FulcioSigner,
-  RekorWitness,
-  TSAWitness,
-} from "@sigstore/sign";
-import { write } from "node:fs";
+import { sign, verify } from "sigstore";
 
 import { readFile, writeFile } from "node:fs/promises"
 
 (async () => {
-  // Set-up the signer
-  const signer = new FulcioSigner({
-    fulcioBaseURL: "https://fulcio.sigstore.dev",
-    identityProvider: new CIContextProvider("sigstore"),
-  });
-
-  // Set-up the witnesses
-  const rekorWitness = new RekorWitness({
-    rekorBaseURL: "https://rekor.sigstore.dev",
-  });
-
-  // Instantiate a bundle builder
-  const bundler = new DSSEBundleBuilder({
-    signer,
-    witnesses: [rekorWitness],
-  });
-
   const artifactBuffer = await readFile("artifact.zip");
 
-  // Sign a thing
-  const artifact = {
-    type: "application/zip",
-    data: artifactBuffer,
-  };
+  const bundle = await sign(artifactBuffer);
+  await writeFile("artifact.zip.sigstore", JSON.stringify(bundle));
 
-  const bundle = await bundler.create(artifact);
-  await writeFile("cosign.bundle", JSON.stringify(bundleToJSON(bundle)))
+  // await verify(bundle, artifactBuffer, {
+  //   certificateIssuer: "1.3.6.1.4.1.57264.1.1",
+  //   certificateIdentityURI: "https://github.com/AshCorr/sigstore-test/.github/workflows/test.yml@refs/heads/main"
+  // });
+
+  console.log("Verified!");
 })();
